16b13d1ea66998e4dadcb77238c7a8fe64493913
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-06-29 13:25:06 +0300

    İlk commit

---
+++ src/main/java/com/springbootpetclinic/SpringBootPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPetClinicApplication.class, args);
+    }
+
+}
