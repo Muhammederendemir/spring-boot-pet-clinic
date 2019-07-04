415fcb28617c1d6683c95ea83d95dcab4bc05e3d
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-07-04 17:16:06 +0300

    TestServelt ve TestFilter oluşturuldu.

--- src/main/java/com/springbootpetclinic/SpringBootPetClinicApplication.java
+++ src/main/java/com/springbootpetclinic/SpringBootPetClinicApplication.java
@@ -3,7 +3,9 @@ package com.springbootpetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 @EnableConfigurationProperties(value = PetClinicProperties.class)
 public class SpringBootPetClinicApplication {
