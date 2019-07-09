1820ec53b33374557d6a5b4f1b45ef8dd1ea8736
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-07-09 13:31:53 +0300

    Test metotları oluşturuldu.

--- src/test/java/com/springbootpetclinic/web/PetClinicRestControllerTest.java
+++ src/test/java/com/springbootpetclinic/web/PetClinicRestControllerTest.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PetClinicRestControllerTest {
     private RestTemplate restTemplate;
 
@@ -22,5 +26,27 @@ public class PetClinicRestControllerTest {
 
         MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
         MatcherAssert.assertThat(response.getBody().getFirstName(), Matchers.equalTo("Muhammed"));
+    }
+
+    @Test
+    public void testOwnerByLastName() {
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8080/rest/owner?ln=Demir", List.class);
+        MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
+        List<Map<String, String>> body = response.getBody();
+        List<String> firstNames = body.stream().map(e -> e.get("firstName")).collect(Collectors.toList()); //Listenin icerisindeki map elemanına ulaşarak
+        // herbır map'in içerisinden lastName eriserek bir string listesine ekliyoruz.
+
+        MatcherAssert.assertThat(firstNames, Matchers.containsInAnyOrder("Muhammed"));//Listenin içerisinde var mı sorusu
+    }
+
+    @Test
+    public void testGetOwners() {
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8080/rest/owners", List.class);
+        MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
+        List<Map<String, String>> body = response.getBody();
+        List<String> firstNames = body.stream().map(e -> e.get("firstName")).collect(Collectors.toList());//Listenin icerisindeki map elemanına ulaşarak
+        // herbır map'in içerisinden lastName eriserek bir string listesine ekliyoruz.
+
+        MatcherAssert.assertThat(firstNames, Matchers.containsInAnyOrder("Muhammed,Ahmet,Yunus,Bekir"));//Listenin içerisinde var mı sorusu
     }
 }
