edc5ea133b2399c64bac170858eba588618ede83
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-07-11 10:27:00 +0300

    schema.sql dosyası içerisinde veritabanına dosyalar oluşturuldu. data.sql dosyası içerisinde oluşturulan tablolara veri eklendi.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into t_owner(id, first_name, last_name)
+values (1, 'Muhammed', 'Demir');
+insert into t_owner(id, first_name, last_name)
+values (2, 'Yunus', 'Delibaş');
+insert into t_owner(id, first_name, last_name)
+values (3, 'Ahmet', 'Şeker');
+
+insert into t_pet(id, name, birth_date, owner_id)
+values (1, 'dog', '2019-09-07', 1);
