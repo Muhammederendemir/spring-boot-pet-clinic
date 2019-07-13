8f6d030cb700ceb614abfe736b4462c419ccd93b
Muhammed Eren Demir <mhmmderen3@gmail.com>
2019-07-13 11:06:10 +0300

    encode işlemi yapıldı ve uygulamaya encode edilen sifre dahil edildi

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,11 +9,11 @@ insert into t_pet(id, name, birth_date, owner_id)
 values (1, 'dog', '2019-09-07', 1);
 
 insert into users
-values ('user1', '{noop}secret', true);
+values ('user1', '{bcrypt}$2a$10$2V1SZ/3Fi7fKfdNNInjRtOe8Wc4t4bJ2FDsxVf7f5keK5VXU7jmLi', true);
 insert into users
-values ('user2', '{noop}secret', true);
+values ('user2', '{bcrypt}$2a$10$OF..v4cGPljtpkrx22rQCenb964NhunrOhEytZ8D2OPhqX74ySNZm', true);
 insert into users
-values ('user3', '{noop}secret', true);
+values ('user3', '{bcrypt}$2a$10$slmc4nW.tN3pePOjRwrRz.LpiaRHptRaQ4sWFFpfw8r6BhBKbFSVm', true);
 
 insert into authorities
 values ('user1', 'ROLE_USER');
---
+++ src/test/java/com/springbootpetclinic/PasswordEncoderTests.java
@@ -0,0 +1,14 @@
+package com.springbootpetclinic;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTests {
+    @Test
+    public void generateEncodedPasswords() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println("{bcrypt}" + passwordEncoder.encode("secret"));
+        System.out.println("{bcrypt}" + passwordEncoder.encode("secret"));
+        System.out.println("{bcrypt}" + passwordEncoder.encode("secret"));
+    }
+}
